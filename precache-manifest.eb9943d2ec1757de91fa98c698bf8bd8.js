9f63c91beef0398cf162300d4d9c91ba855b6454
KovtunAnton <a.kovtun@rgkgroup.com>
2019-05-24 17:29:07 +0300

    Updates

---
+++ precache-manifest.eb9943d2ec1757de91fa98c698bf8bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68abbdd221d0f78befc6cc635d73ceb",
+    "url": "/starnavi/index.html"
+  },
+  {
+    "revision": "c79defc32c206b6d57d5",
+    "url": "/starnavi/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1396229bbf7adcdc7e64",
+    "url": "/starnavi/static/css/main.f44e72fa.chunk.css"
+  },
+  {
+    "revision": "c79defc32c206b6d57d5",
+    "url": "/starnavi/static/js/2.63d99022.chunk.js"
+  },
+  {
+    "revision": "1396229bbf7adcdc7e64",
+    "url": "/starnavi/static/js/main.6a49125a.chunk.js"
+  },
+  {
+    "revision": "b38a2eb7b467d87605f3",
+    "url": "/starnavi/static/js/runtime~main.29db0d93.js"
+  }
+]);
